9cfb94b40b17719b7d17ca46c9849c3b92e38b08
Josie Bröhm <121027909+josiebrohm@users.noreply.github.com>
2022-12-19 17:45:52 +0200

    added NoRoute page for robustness

--- hatchr-react-app/src/App.js
+++ hatchr-react-app/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Companies from './pages/Companies';
 import Assessments from './pages/Assessments';
+import NoRoute from './pages/NoRoute';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />}/> 
         <Route path="/companies" element={<Companies />}/>
         <Route path="/assessments" element={<Assessments />}/>
+        <Route path='*' element={<NoRoute/>} />
       </Routes>
     </div>
   );
---
+++ hatchr-react-app/src/pages/NoRoute.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NoRoute() {
+    return (
+        <div>
+            <h1>Oops!</h1>
+            <h3>The page you are looking for doesn't seem to exist!</h3>
+            <Link to="/">Click here</Link>
+        </div>
+    )
+}
+export default NoRoute;
