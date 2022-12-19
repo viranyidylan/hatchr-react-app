4d107b2248f4431746894c7cab978548c1062b3f
Josie Bröhm <121027909+josiebrohm@users.noreply.github.com>
2022-12-19 17:32:14 +0200

    added home page and fixed routing issue

--- hatchr-react-app/src/App.js
+++ hatchr-react-app/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hatchr</h1>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />}/> 
+      </Routes>
     </div>
   );
 }
---
+++ hatchr-react-app/src/pages/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="home-page">
+            <header className="App-header">
+                <h1>hatchr</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
