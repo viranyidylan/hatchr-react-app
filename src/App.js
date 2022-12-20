436d320586ac0a201141f7c6bd052d6e2e448f3a
josiebrohm <121027909+josiebrohm@users.noreply.github.com>
2022-12-20 16:29:24 +0200

    added home page, companies and assessment page components (#3)
    * added home page and fixed routing issue
    
    * added home, assessments, and companies pages
    
    * added NoRoute page for robustness
    
    * added home page, companies and assessment page components
    
    Co-authored-by: Dylan Viranyi <dylan.viranyi@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Companies from './pages/Companies';
+import Assessments from './pages/Assessments';
+import NoRoute from './pages/NoRoute';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hatchr</h1>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />}/> 
+        <Route path="/companies" element={<Companies />}/>
+        <Route path="/assessments" element={<Assessments />}/>
+        <Route path='*' element={<NoRoute/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Assessments.js
@@ -0,0 +1,11 @@
+function Assessments() {
+    return (
+        <div className="home-page">
+            <header className="App-header">
+                <h1>this is the assessments page</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Assessments;
