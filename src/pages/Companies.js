3a8d80873902dc95d5201609d11e138a4a9f3231
Josie Bröhm <121027909+josiebrohm@users.noreply.github.com>
2022-12-20 13:42:42 +0200

    added home page, companies and assessment page components

---
+++ src/pages/Companies.js
@@ -0,0 +1,11 @@
+function Companies() {
+    return (
+        <div className="home-page">
+            <header className="App-header">
+                <h1>this is the companies page</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Companies;
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div className="home-page">
+            <header className="App-header">
+                <h1>this is the homepage</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
