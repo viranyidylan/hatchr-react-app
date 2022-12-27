c65cd6e9086f4349cce6748cc43a9f25f881399c
Josie Bröhm <121027909+josiebrohm@users.noreply.github.com>
2022-12-27 17:11:11 +0200

    added hatchr logo to navbar

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,11 +1,15 @@
-import { Button, Container, Form, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Button, Container, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap'
+//import { Link } from "react-router-dom";
+import {ReactComponent as Logo} from '../assets/hatchr-logo.svg'
+
 
 function MenuBar() {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
-            <Container>
-                <Navbar.Brand href="/">Hatchr</Navbar.Brand> {/* will add logo here! */}
+        <Navbar collapseOnSelect expand="lg" bg="dark">
+            <Container >
+                <Navbar.Brand href="/">
+                    <Logo height='45px'/>
+                </Navbar.Brand> {/* will add logo here! */}
                 <Navbar.Toggle aria-controls="offcanvasNavbar-expand-${expand}" />
                 <Navbar.Offcanvas 
                     id={`offcanvasNavbar-expand-${"lg"}`}
@@ -20,21 +24,15 @@ function MenuBar() {
                         <Nav className="justify-content-end flex-grow-1 pe-3">
                             <Nav.Link href="/companies">Companies</Nav.Link>
                             <Nav.Link href="/assessments">Assessments</Nav.Link>
-                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="">things</NavDropdown.Item>
                                 <NavDropdown.Item href="">for</NavDropdown.Item>
                                 <NavDropdown.Item href="">future</NavDropdown.Item>
                                 <NavDropdown.Item href="">
                                     use
                                 </NavDropdown.Item>
-                            </NavDropdown>
+                            </NavDropdown> */}
                         </Nav>
-                        {/* <Nav>
-                            <Nav.Link href="#deets">More deets</Nav.Link>
-                            <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                            </Nav.Link>
-                        </Nav> */}
                     </Offcanvas.Body>
                 </Navbar.Offcanvas>
             </Container>
