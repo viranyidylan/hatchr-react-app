ab5f0630020186b3d169ec189d1e8c92107bb2a4
Josie Bröhm <121027909+josiebrohm@users.noreply.github.com>
2023-01-20 10:33:34 +0200

    added button box style and hover, still need to center align buttons in the box

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 
 function Home() {
     return (
@@ -7,18 +7,27 @@ function Home() {
             <div className="home-page-background-1">
                 <div className="home-page-container-1">
                     <Row className="home-page-title">
-                        <h1>Software engineering interviews are <span style={{color: "#f70462"}}>broken</span>. Hatchr is fixing the process.</h1>
+                        <h1>Software engineering interviews are <span style={{color: "#de1867"}}>broken</span>. Hatchr is fixing the process.</h1>
                     </Row>
                     <Row className="home-page-subtitle">
                         <h2>Hatchr is the platform for you to find your next software engineering job. 
                         Forget studying for coding whiteboards, memorising obscure algorithms questions and bragging about what university you went to. We focus on what actually matters for software engineering - your ability to build things with code.</h2>
                     </Row>
-                    <Row>
-                        <h3>button box!</h3>
+                    <Row className="button-box">
+                        <Col>
+                            <Button id="btn-custom-primary" variant="primary">
+                                <h2>Get started</h2>
+                            </Button>
+                        </Col>
+                        <Col>
+                            <Button id="btn-custom-secondary" variant="secondary">
+                                <h2>See coding tasks</h2>
+                            </Button>
+                        </Col>
                     </Row>
                 </div>
                 <div className="home-page-container-2">
-                    
+
                 </div>
             </div>
         </div>
