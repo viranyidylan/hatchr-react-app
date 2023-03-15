3d82a9499e5fd6a392a3aa43b217c637074df8f0
JOSIE_BROHM <121027909+josiebrohm@users.noreply.github.com>
2023-03-15 13:47:47 +0200

    added footer component

--- src/components/MenuBar/MenuBar.tsx
+++ src/components/MenuBar/MenuBar.tsx
@@ -16,58 +16,58 @@ if (window.location.pathname === '/companies') {
 }
 
 function MenuBar() {
-    const [hidden, setHidden] = useState(false);
-    const [expanded, setExpanded] = useState(false);
-    // const [loggedIn, setLoggedIn] = useState(false);
+  const [hidden, setHidden] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  // const [loggedIn, setLoggedIn] = useState(false);
 
-    const navControl = () => {
-        if (typeof window !== 'undefined') {
-            if (window.scrollY < 70) {
-                setHidden(false);
-            } else {
-                setHidden(true);
-                setExpanded(false);
-            }
-        }
-    };
+  const navControl = () => {
+      if (typeof window !== 'undefined') {
+          if (window.scrollY < 70) {
+              setHidden(false);
+          } else {
+              setHidden(true);
+              setExpanded(false);
+          }
+      }
+  };
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            window.addEventListener('scroll', navControl);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', navControl);
 
-            // cleanup function
-            return () => {
-                window.removeEventListener('scroll', navControl);
-            };
-        }
-    }, []);
+      // cleanup function
+      return () => {
+          window.removeEventListener('scroll', navControl);
+      };
+    }
+  }, []);
 
-    const expansionControl = () => {
-        setExpanded(!expanded);
-    };
+  const expansionControl = () => {
+      setExpanded(!expanded);
+  };
 
-    return (
-        <Navbar className="menu-bar" onToggle={expansionControl} expand="lg" variant='dark' fixed="top" hidden={hidden} expanded={expanded} >
-            <Container className="menu-bar-container">
-                <Navbar.Brand>
-                    <Nav.Link href='/'>
-                        <Logo height='40px' width='200px' />
-                    </Nav.Link>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="menu-links">
-                        <Nav.Link className='nav-link' href={navButtonPath}><h3>{"For " + navButtonText}</h3></Nav.Link>
-                        <Nav.Link className='nav-link' href="/assessments"><h3>Coding tasks</h3></Nav.Link>
-                        {/* <Nav.Link className='nav-link' href="/assessments"><h3>Log in</h3></Nav.Link> */}
-                        {/* <Button className='nav-button' id="btn-custom-primary" variant="primary">
-                            <h3>{loginText}</h3>
-                        </Button> */}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+      <Navbar className="menu-bar" onToggle={expansionControl} expand="lg" variant='dark' fixed="top" hidden={hidden} expanded={expanded} >
+          <Container className="menu-bar-container">
+              <Navbar.Brand>
+                  <Nav.Link href='/'>
+                      <Logo height='40px' width='200px' />
+                  </Nav.Link>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="menu-links">
+                      <Nav.Link className='nav-link' href={navButtonPath}><h3>{"For " + navButtonText}</h3></Nav.Link>
+                      <Nav.Link className='nav-link' href="/assessments"><h3>Coding tasks</h3></Nav.Link>
+                      {/* <Nav.Link className='nav-link' href="/assessments"><h3>Log in</h3></Nav.Link> */}
+                      {/* <Button className='nav-button' id="btn-custom-primary" variant="primary">
+                          <h3>{loginText}</h3>
+                      </Button> */}
+                  </Nav>
+              </Navbar.Collapse>
+          </Container>
+      </Navbar>
+  );
 }
 
 export default MenuBar;
